660a23c1cb1d3aac6ce63e66d46c5292a0640cda
gianlucadenunzio <gianluca.denunzio@digitouch.it>
2025-02-25 10:04:28 +0100

    Fix format date (#7)
    Co-authored-by: gdenunzio <gianluca.denunzio@mobilesoft.it>

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,12 +1,11 @@
 import { format } from "date-fns";
-import { it } from "date-fns/locale";
 
 export function formatCustomDate(dateString: string | Date): string {
-    const date = typeof dateString === "string" ? new Date(dateString) : dateString;
-  
-    if (isNaN(date.getTime())) {
-      throw new Error("Invalid date");
-    }
+  const date = typeof dateString === "string" ? new Date(dateString) : dateString;
 
-    return format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", { locale: it });
+  if (isNaN(date.getTime())) {
+    throw new Error("Invalid date");
+  }
+
+  return format(date, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 }
